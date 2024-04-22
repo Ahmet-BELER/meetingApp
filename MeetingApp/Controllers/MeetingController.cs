a65e42a4c61f9ff1bd9157919897b4abb651c434
ahmet <ahmet.beler@haberler.com>
2024-04-22 17:31:38 +0300

    detail

--- MeetingApp/Controllers/MeetingController.cs
+++ MeetingApp/Controllers/MeetingController.cs
@@ -1,29 +1,43 @@
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
+using MeetingApp.Models;
 
 namespace MeetingApp.Controllers
 {
-  
     public class MeetingController : Controller
     {
-   
 
-        public IActionResult  Index()
+
+        [HttpGet]
+
+        public IActionResult Apply()
         {
             return View();
         }
-               public IActionResult  Apply()
+
+        [HttpPost]
+
+        public IActionResult Apply(UserInfo model)
+
         {
-            return View();
+            Repository.CreateUser( model);
+            ViewBag.UserCount = Repository.Users.Where(info => info.WillAttend == true).Count();
+            return View("Thanks", model);
+
         }
-                public IActionResult  List()
+
+        [HttpGet]
+
+        public IActionResult List()
         {
-            return View();
+            return View(Repository.Users);
+        }
+
+
+       [HttpGet]
+
+        public IActionResult Details(int id)
+        {
+            return View(Repository.GetById(id));
         }
 
 
